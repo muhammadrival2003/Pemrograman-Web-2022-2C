4664a52cd5315ee66a1905086cdf8d61d02ae155
muhammadrival2003 <muhammadrival040303@gmail.com>
2022-10-09 11:14:55 +0700

    Tugas MySQL

---
+++ Praktikum_5/MYSql/view.php
@@ -0,0 +1,22 @@
+<?php
+    $conn = mysqli_connect("locahost", "root", "", "db_web2022");
+    $hasil = mysqli_query($conn, "select * from bukutamu");
+    $jumlah = mysqli_num_rows($hasil);
+    echo "<center>Daftar pengunjung</center>";
+    echo "Jumlah pengunjung : $jumlah";
+    $a=1;
+    while ($baris = mysqli_fetch_array(($hasil))){
+        echo "<br>";
+        echo $a;
+        echo "<br>";
+        echo "Nama : ";
+        echo $baris[0];
+        echo "<br>";
+        echo "Email : ";
+        echo $baris[1];
+        echo "<br>";
+        echo "Email : ";
+        echo $baris[2];
+        $a++;
+    }
+?>
